d82b9fcd3b6e48bab2932ae756f169892969c6a0
mohitjoer <mohitjoe.r@gmail.com>
2025-08-30 23:26:28 +0530

    commit	modified:   main.py commit	modified:   pyproject.toml commit	modified:   uv.lock

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from learning-ml!")
+from sklearn.datasets import fetch_california_housing
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+import matplotlib.pylab as plt
 
+x, y = fetch_california_housing(return_X_y=True)
 
-if __name__ == "__main__":
-    main()
+model = KNeighborsRegressor()
+model.fit(x, y)
+
+pred=model.predict(x)
+
+plt.scatter(pred , y)
+plt.show()
